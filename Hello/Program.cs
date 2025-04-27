8b73de4e58d42fbe20ca1912cee5ce6317cf1074
Moxika Vaghela <moxikavaghela15123@gmail.com>
2025-04-27 20:09:32 +0530

    Performing various functions and leaning different aspects

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -66,6 +66,11 @@ namespace Hello //namespace is used to organize your code, and it is a container
                    size:4 bytes
              */
 
+            //Constants:
+            //Constants are variables whose values cannot be changed during the execution of the program. In C#, you can declare a constant using the const keyword. For example, if you want to declare a constant named PI with a value of 3.14, you can do it like this const double PI= 3.14;:
+            //const int H = 10;
+            //int H = 20; //This will give an error because H is a constant and its value cannot be changed.
+
             //Declaring variables:
             //DataType VariableName = value;
 
@@ -146,6 +151,134 @@ namespace Hello //namespace is used to organize your code, and it is a container
             //This will give an error because you cannot assign a double value to an int variable without type-casting.
             //int xyz = (int)1.5; //This will convert the double value 1.5 to an int value 1, and assign it to the variable a.
             //Console.WriteLine(xyz); //This will print the value of a to the console.
+
+            //Type-Casting using Convert Class and default methods:
+            //Convert.ToBoolean, Convert.ToDouble, Convert.ToString, Convert.ToInt32 (int) ,Convert.ToInt64 (long)
+
+            //You can also use the Convert class to convert between different data types. For example, if you want to convert a string to an int, you can do it like this:
+            //string str = "134"; //This declares a variable named str of type string and assigns the value "123" to it.
+            //int num = Convert.ToInt32(str); //This converts the string value of str to an int value and assigns it to the variable num.
+            //Console.WriteLine(num); //This will print the value of num to the console.
+
+            //Taking Input from the User:
+            //You can also take input from the user using the Console.ReadLine() method. For example, if you want to take an int input from the user, you can do it like this:
+
+            //Console.WriteLine("Enter an Name:"); //This will prompt the user to enter an integer.
+            //string name = Console.ReadLine(); //This will read the input from the user and store it in the variable name.
+            //Console.WriteLine("Hello " + name); //This will print "Hello " followed by the name entered by the user.
+            //Console.ReadLine();
+
+            //Console.WriteLine("How Many Candies Do you want?"); //This will prompt the user to enter an integer.
+            //string can = Console.ReadLine(); //This will read the input from the user and store it in the variable name.
+            //Console.WriteLine("you will get 5 more candies " +(Convert.ToInt32(can) + 5) + " in few minutes."); //This will print "Hello " followed by the name entered by the user.
+
+            //Operators:
+            //Operators are used to perform operations on variables and values. In C#, there are different types of operators, such as:
+
+            //Arithmetic operators: +, -, *, /, %, ++, --
+            //Unary operators: +, -, ++, --
+
+            //+ is used to add two numbers.
+            //- is used to subtract two numbers.
+            //* is used to multiply two numbers.
+            // / is used to divide two numbers.
+            // % is used to get the remainder of a division operation.
+            // ++ is used to increment a variable by 1.
+            // -- is used to decrement a variable by 1.
+            //For example:
+
+            //int a = 15; //This declares a variable named a of type int and assigns the value 5 to it.
+            //int b = 4;//This declares a variable named b of type int and assigns the value 5 to it.
+
+            //Console.WriteLine("Arithmatic: "+ (a + b)); //This will print the sum of a and b to the console.
+            //Console.WriteLine("Subtract: "+(a - b)); //This will print the difference of a and b to the console.
+            //Console.WriteLine("Multiply: "+(a * b)); //This will print the product of a and b to the console.
+            //Console.WriteLine("Division: "+(a / b)); //This will print the quotient of a and b to the console.
+            //Console.WriteLine("Modular: "+(a % b)); //This will print the remainder of a divided by b to the console.
+            //Console.WriteLine("Increment: " +(a++)); //This will print the value of a and then increment it by 1.
+            //Console.WriteLine("Decrement: " + (b--)); //This will print the new value of a to the console.
+
+
+
+            //Assignment operators: =, +=, -=, *=, /=, %=
+            //= is used to assign a value to a variable.
+            //+= is used to add a value to a variable and assign the result to the variable.
+            //-= is used to subtract a value from a variable and assign the result to the variable.
+            //*= is used to multiply a variable by a value and assign the result to the variable.
+            // /= is used to divide a variable by a value and assign the result to the variable.
+            // %= is used to get the remainder of a division operation and assign the result to the variable.
+            //For example:
+            //int p = 16; //This declares a variable named p of type int and assigns the value 5 to it.
+            //int q = 3; //This declares a variable named q of type int and assigns the value 10 to it.
+
+            //Console.WriteLine("Addition Assignment: " + (p += q)); //This will print the value of p after adding the value of q to it.
+            //Console.WriteLine("Subtraction Assignment: " + (p -= q)); //This will print the value of p after subtracting the value of q from it.
+            //Console.WriteLine("Multiplication Assignment: " + (p *= q)); //This will print the value of p after multiplying it by the value of q.
+            //Console.WriteLine("Division Assignment: " + (p /= q)); //This will print the value of p after dividing it by the value of q.
+            //Console.WriteLine("Modular Assignment: " + (p %= q)); //This will print the value of p after getting the remainder of p divided by q.
+            //Console.WriteLine("Assignment: " + (p = q)); //This will print the value of p after assigning the value of q to it.
+
+
+            //Comparison operators: ==, !=, >, <, >=, <=
+            //== is used to check if two values are equal.
+            //!= is used to check if two values are not equal.
+            //> is used to check if a value is greater than another value.
+            //< is used to check if a value is less than another value.
+            //>= is used to check if a value is greater than or equal to another value.
+            //<= is used to check if a value is less than or equal to another value.
+            //For example:
+            //int m = 8; //This declares a variable named x of type int and assigns the value 8 to it.
+            //int n = 10; //This declares a variable named y of type int and assigns the value 10 to it.
+            //Console.WriteLine("Equal: " + (m == n)); //This will print true if m is equal to n, otherwise false.
+            //Console.WriteLine("Not Equal: " + (m != n)); //This will print true if m is not equal to n, otherwise false.
+            //Console.WriteLine("Greater Than: " + (m > n)); //This will print true if m is greater than n, otherwise false.
+            //Console.WriteLine("Less Than: " + (m < n)); //This will print true if m is less than n, otherwise false.
+            //Console.WriteLine("Greater Than or Equal To: " + (m >= n)); //This will print true if m is greater than or equal to n, otherwise false.
+            //Console.WriteLine("Less Than or Equal To: " + (m <= n)); //This will print true if m is less than or equal to n, otherwise false.
+
+
+            //Logical operators: &&, ||, !
+            //&& is used to check if two conditions are true.
+            //|| is used to check if at least one of two conditions is true.
+            //! is used to check if a condition is false.
+            //For example:
+            bool x = true; //This declares a variable named x of type bool and assigns the value true to it.
+            bool y = false; //This declares a variable named y of type bool and assigns the value false to it.
+            Console.WriteLine("Logical AND: " + (x && y)); //This will print true if both x and y are true, otherwise false.
+            Console.WriteLine("Logical OR: " + (x || y)); //This will print true if either x or y is true, otherwise false.
+            Console.WriteLine("Logical NOT: " + (!x)); //This will print true if x is false, otherwise false.
+
+
+            //Bitwise operators: &, |, ^, ~, <<, >>
+            //& is used to perform a bitwise AND operation.
+            //| is used to perform a bitwise OR operation.
+            //^ is used to perform a bitwise XOR operation.
+            //~ is used to perform a bitwise NOT operation.
+            //<< is used to perform a left shift operation.
+            //>> is used to perform a right shift operation.
+            //For example:
+            //int a1 = 5; //This declares a variable named a of type int and assigns the value 5 to it.
+            //int b1 = 3; //This declares a variable named b of type int and assigns the value 3 to it.
+            //Console.WriteLine("Bitwise AND: " + (a1 & b1)); //This will print the result of a1 AND b1.
+            //Console.WriteLine("Bitwise OR: " + (a1 | b1)); //This will print the result of a1 OR b1.
+            //Console.WriteLine("Bitwise XOR: " + (a1 ^ b1)); //This will print the result of a1 XOR b1.
+            //Console.WriteLine("Bitwise NOT: " + (~a1)); //This will print the result of NOT a1.
+            //Console.WriteLine("Left Shift: " + (a1 << 1)); //This will print the result of a1 left shifted by 1 bit.
+            //Console.WriteLine("Right Shift: " + (a1 >> 1)); //This will print the result of a1 right shifted by 1 bit.
+            
+            //Conditional operators: ?:, ??, ?[] , ?()
+            //?: is used to check if a condition is true or false and return a value based on the result.
+            //?? is used to check if a value is null and return a default value if it is.
+            //?[] is used to check if an array element is null and return a default value if it is.
+            //?() is used to check if a method returns null and return a default value if it does.
+            //Null-coalescing operator: ??
+            //Null-conditional operator: ?.
+            //Null-coalescing assignment operator: ??=
+            //Null-conditional assignment operator: ?=
+
+            //Ternary operator: ? :
+            //The ternary operator is a shorthand way of writing an if-else statement. It takes three operands: a condition, a value to return if the condition is true, and a value to return if the condition is false. For example:
+
 
             Console.ReadLine();
         }
